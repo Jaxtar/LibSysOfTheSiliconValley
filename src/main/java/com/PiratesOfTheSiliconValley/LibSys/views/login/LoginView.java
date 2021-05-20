0fe3275f058f55f7460b748d36e0965b6fb01147
MusaeM <Mustafa.Marouf@hotmail.com>
2021-05-20 10:02:25 +0200

    Fix Login-layout

--- src/main/java/com/PiratesOfTheSiliconValley/LibSys/views/login/LoginView.java
+++ src/main/java/com/PiratesOfTheSiliconValley/LibSys/views/login/LoginView.java
@@ -5,6 +5,7 @@ import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.component.textfield.TextField;
@@ -15,8 +16,14 @@ import com.vaadin.flow.router.Route;
 @Route(value = "login")
 @PageTitle("Login")
 public class LoginView extends VerticalLayout {
+
         public LoginView(AuthService authService) {
             setId("login-view");
+
+            setSizeFull();
+            setAlignItems(Alignment.CENTER);
+            setJustifyContentMode(JustifyContentMode.CENTER);
+
             TextField username = new TextField("Username");
             PasswordField password = new PasswordField("Password");
             add(
